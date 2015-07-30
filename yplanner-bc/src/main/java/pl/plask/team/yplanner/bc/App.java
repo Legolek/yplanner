a22d4e792e02a3c31a066a9e73c6180865491661
Michał Łęgocki <mlegocki@gmail.com>
2015-07-30 22:18:43 +0200

    Parent and bc project created

---
+++ yplanner-bc/src/main/java/pl/plask/team/yplanner/bc/App.java
@@ -0,0 +1,13 @@
+package pl.plask.team.yplanner.bc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
