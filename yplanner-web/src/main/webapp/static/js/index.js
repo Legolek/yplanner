479b8ed53bc93dec7024e2f247f707788680ab79
Michał Łęgocki <mlegocki@gmail.com>
2015-08-03 02:07:35 +0200

    Login screen

--- yplanner-web/src/main/webapp/static/js/index.js
+++ yplanner-web/src/main/webapp/static/js/index.js
@@ -1,5 +1,9 @@
 var app = angular.module("index", []);
 
 app.controller("LoginController", function() {
-	this.welcomeMsg = "Welcome!";
+	this.login = null;
+	this.password = null;
+	this.submitLogin = function() {
+		alert(this.login + " " + this.password);
+	};
 });
