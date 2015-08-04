8864c9dd03edf595890b02f3107e1d4853931b60
Michał Łęgocki <mlegocki@gmail.com>
2015-08-05 00:58:05 +0200

    Added localization config + changes in web-app config

---
+++ yplanner-web/src/main/java/pl/plask/team/yplanner/web/controllers/resources/MessageController.java
@@ -0,0 +1,36 @@
+package pl.plask.team.yplanner.web.controllers.resources;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+	
+	@Resource(name = "messagesProperties")
+	private Properties messages;
+
+	@RequestMapping(value = "/res/messages.js",  method = RequestMethod.GET)
+	public Map<String, String> loadMessages() {
+		Map<String, String> map = new HashMap<>();
+		for (String key : messages.stringPropertyNames()) {
+			map.put(key, messages.getProperty(key));
+		}
+		return map;
+	}
+
+	public Properties getMessages() {
+		return messages;
+	}
+
+	public void setMessages(Properties messages) {
+		this.messages = messages;
+	}
+	
+}
--- yplanner-web/src/main/webapp/static/js/index.js
+++ yplanner-web/src/main/webapp/static/js/index.js
@@ -1,9 +1,24 @@
-var app = angular.module("index", []);
+var app = angular.module("index", ['ngRoute', 'ngResource']);
 
-app.controller("LoginController", function() {
+app.service("MessagesService", function($resource) {
+	var messages = {};
+	return {
+		loadMessages: function() {
+			$resource('res/messages.js').get(function(data) {
+				angular.copy(data, messages);
+			});
+		},
+		messages: messages
+	}
+});
+
+app.controller("LoginController", function($resource, MessagesService) {
 	this.login = null;
 	this.password = null;
 	this.submitLogin = function() {
 		alert(this.login + " " + this.password);
 	};
+	
+	this.msg = MessagesService.messages;
+	MessagesService.loadMessages();
 });
