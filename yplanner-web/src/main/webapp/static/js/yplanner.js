a0270a078a7484213477f535605aacdb7af41c63
Michał Łęgocki <mlegocki@gmail.com>
2015-09-25 00:19:25 +0200

    Menu customization

--- yplanner-web/src/main/webapp/static/js/yplanner.js
+++ yplanner-web/src/main/webapp/static/js/yplanner.js
@@ -1,12 +1,16 @@
 var app = angular.module("yplanner", [ "msg", "ngRoute", "ui.bootstrap"]);
 
 app.controller("MenuController", function($scope, $location) {
-	$scope.isActive = function(view) {
+	$scope.isActive = function(view, submenu) {
 		if (view === '/' && $location.path() === '') {
 			return true;
 		}
-		var splitArr = $location.path().split("/");
-		return (view === "/" + splitArr[1]);
+		if (!submenu) {
+			var splitArr = $location.path().split("/");
+			return (view === "/" + splitArr[1]);
+		} else {
+			return view === $location.path();
+		}
 	}
 });
 
